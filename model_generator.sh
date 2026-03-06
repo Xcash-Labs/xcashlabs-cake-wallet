875ec391c327d1094e65dd04462efa9ace54780a
John D <42452856+minerjed@users.noreply.github.com>
2026-03-06 12:06:10 -0500

    Only builld monero

--- model_generator.sh
+++ model_generator.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 set -x -e
 
-for cwcoin in cw_{core,evm,monero,bitcoin,nano,bitcoin_cash,solana,tron,wownero,zano,decred,dogecoin,zcash}
+for cwcoin in cw_{core,evm,monero}
 do
     if [[ "x$1" == "xasync" ]];
     then
         bash -c "cd $cwcoin; flutter pub get; dart run build_runner build --delete-conflicting-outputs; cd .." &
     else
         cd $cwcoin; flutter pub get; dart run build_runner build --delete-conflicting-outputs; cd ..
-    fi
-done
-for cwcoin in cw_mweb;
-do
-    if [[ "x$1" == "xasync" ]];
-    then
-        bash -c "cd $cwcoin; flutter pub get; cd .." &
-    else
-        cd $cwcoin; flutter pub get; cd ..
     fi
 done
 
